393b9d872229832583b74f138388c453c952657b
gon-zo <pnw123456@icloud.com>
2021-12-17 20:02:53 +0900

    fix: Kotlin DSL Build Error #7

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,20 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
+import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
     id("org.springframework.boot") version "2.6.1"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.0"
     kotlin("plugin.spring") version "1.6.0"
-    id("org.springframework.experimental.aot") version "0.11.0"
+
+    kotlin("plugin.allopen") version "1.4.32"
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.Embeddable")
+    annotation("javax.persistence.MappedSuperclass")
 }
 
 group = "com.weasleyclock"
@@ -20,30 +28,22 @@ repositories {
 
 dependencies {
 
+    implementation("org.springframework.boot:spring-boot-starter-mustache")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-
-    //swagger
     implementation("io.springfox:springfox-boot-starter:3.0.0")
-    //logger
     implementation("io.github.microutils:kotlin-logging-jvm:2.0.6")
-    //jasypt
-    implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:1.17")
-    //Mysql
+    implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.4")
     implementation("mysql:mysql-connector-java")
-    //JPA
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    //H2
     implementation("com.h2database:h2")
     runtimeOnly("com.h2database:h2")
     implementation("org.apache.tomcat:tomcat-jdbc:7.0.19")
-    // utils
     implementation("org.apache.commons:commons-lang3:3.9")
-
     implementation("org.springframework.boot:spring-boot-starter-security")
 
 }
@@ -51,7 +51,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+        jvmTarget = "13"
     }
 }
 
@@ -62,4 +62,8 @@ tasks.withType<Test> {
 tasks.withType<BootBuildImage> {
     builder = "paketobuildpacks/builder:tiny"
     environment = mapOf("BP_NATIVE_IMAGE" to "true")
+}
+
+tasks.getByName<BootJar>("bootJar") {
+    enabled = true
 }
