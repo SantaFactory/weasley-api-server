87fcbba6502f8b287e7ac93fd0d7b5dcdb80c76e
gon-zo <pnw123456@icloud.com>
2021-12-26 20:48:19 +0900

    chore: move package jpa config #18

--- src/main/kotlin/com/weasleyclock/weasley/config/JpaConfiguration.kt
+++ src/main/kotlin/com/weasleyclock/weasley/config/JpaConfiguration.kt
@@ -1,5 +1,6 @@
-package com.weasleyclock.weasley.config.security
+package com.weasleyclock.weasley.config
 
+import com.weasleyclock.weasley.config.security.DomainAuditorAware
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
